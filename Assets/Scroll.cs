ef69165f675af67a37367fa295f36cae42cd67ec
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-19 01:26:19 +0900

    배경화면이 -1 방향으로 이동

--- Assets/Scroll.cs
+++ Assets/Scroll.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float SppedX = -1;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(SppedX * Time.deltaTime,0,0);
     }
 }
