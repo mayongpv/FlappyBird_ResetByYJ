239d0e06b6542294819d8488a3a2b8901aee0d7c
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-19 17:48:47 +0900

    Column Spawn

---
+++ Assets/Column.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    //씬이 시작되면
+    //랜덤으로 생성하기 - 랜덤 함수 필요
+    //Y 값의 범위 있어야함. 
+    //x 값은 고정값?
+
+
+    //// 3초 간격으로 배치 : 코르틴 함수 사용하기
+
+    public GameObject column;
+    public float delay = 3f;
+    public float randomdelay = 2f;
+    public float spawnX = 4;
+    public float spawnYmin = 4;
+    public float spawnYmax = 4;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            Vector3 pos;
+            pos.x = spawnX;
+            pos.y = Random.Range(spawnYmin, spawnYmax)  ;
+            pos.z = 0;
+
+
+            Instantiate(column, pos, column.transform.rotation);
+
+            //3초 간격으로 배치
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+
+}
